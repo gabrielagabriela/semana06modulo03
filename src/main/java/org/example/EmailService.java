65a6b5f90e6e9ce12bbf574b76ffa92f113c5cc4
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-11-11 10:18:44 -0300

    ex 05

---
+++ src/main/java/org/example/EmailService.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class EmailService {
+    public void enviarEmail(String email) {
+        System.out.println("Email enviado para " + email);
+    }
+}
---
+++ src/main/java/org/example/UsuarioService.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class UsuarioService {
+    public void adicionarUsuario(String nome) {
+        System.out.println("Usuário " + nome + " adicionado com sucesso.");
+    }
+}
